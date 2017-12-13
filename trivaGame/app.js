86d16545cc321a23dda471dc195c58687a76e0bf
pdwilliamson5 <30446985+pdwilliamson5@users.noreply.github.com>
2017-12-13 15:42:08 -0600

    Add files via upload

--- trivaGame/app.js
+++ trivaGame/app.js
@@ -1,3 +1,23 @@
+
+
+
+var count = 20;
+var counter=setInterval(timer, 1000);
+
+function timer()
+{
+	count=count-1;
+	if (count <= 0)
+	{	clearInterval(counter);
+		alert("Boom you are dead!")
+		return;
+		
+	}
+	document.getElementById("timer").innerHTML=count + "secs";
+}
+
+
+
 function check()
 {	var q1 = document.quiz.group1.value;
 	var q2 = document.quiz.group2.value;
@@ -23,7 +43,8 @@ function check()
 		count--;
 	}
 	if(count == 3){
-		alert("You're getting to old for this sh*t");
+		alert("You're getting to old for this sh*t!");
+		
 	}
 	else{
 		alert("Boom you're dead!");
@@ -31,17 +52,3 @@ function check()
 
 }
 
-var counter = 0;
-var timeleft = 20;
-function setup {
-	noCanvas();
- var timer = select('#clock');
-		timer.html('0');
-
-		function timeIt(){
-			counter ++;
-			timer.html(timeleft - counter);
-
-		}
-		setInterval(timeIt, 1000);
-	}
